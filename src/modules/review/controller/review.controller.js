32be9610646eeaacd127bba72a784804e7f499be
HudaNady <hudanady9@gmail.com>
2025-04-16 16:24:48 +0200

    update review

--- src/modules/review/controller/review.controller.js
+++ src/modules/review/controller/review.controller.js
@@ -5,8 +5,8 @@ import roles from "../../../types/roles.js";
 import Product from "../../../../DB/models/Product.js";
 
 export const addReview = asyncHandler(async (req, res, next) => {
-    req.body.user = req.user.userId;
-    const reviewExist = await Review.findOne({ user: req.user.userId, product: req.body.product });
+    req.body.user = req.user._id;
+    const reviewExist = await Review.findOne({ user: req.user._id, product: req.body.product });
     if (reviewExist) {
         return next(new AppError("You have already reviewed this product", 409));
     }
@@ -15,6 +15,8 @@ export const addReview = asyncHandler(async (req, res, next) => {
         return next(new AppError("Product not found", 404));
     }
     const review = await Review.create(req.body);
+    product.reviews.push(review._id);
+    await product.save();
     return res.status(201).json({ message: "Review added successfully", review });
 });
 
@@ -26,20 +28,14 @@ export const getAllReviews = asyncHandler(async (req, res, next) => {
     return next(new AppError("Reviews not found", 404));
 });
 
-export const getReviewById = asyncHandler(async (req, res, next) => {
-    const review = await Review.findById(req.params._id);
-    if (review) {
-        return res.status(200).json({ message: "Review retrieved successfully", review });
-    }
-    return next(new AppError("Review not found", 404));
-});
+
 
 export const updateReview = asyncHandler(async (req, res, next) => {
     const review = await Review.findById(req.params._id);
     if (!review) {
         return next(new AppError("Review not found", 404));
     }
-    if (review.user.toString() === req.user.userId || req.user.rol === roles.admin) {
+    if (review.user.toString() === req.user._id || req.user.rol === roles.admin) {
         const updatedReview = await Review.findByIdAndUpdate(req.params._id, req.body, { new: true });
         return res.status(200).json({ message: "Review updated successfully", review: updatedReview });
     }
@@ -51,7 +47,7 @@ export const deleteReview = asyncHandler(async (req, res, next) => {
     if (!review) {
         return next(new AppError("Review not found", 404));
     }
-    if (review.user.toString() === req.user.userId || req.user.rol === roles.admin) {
+    if (review.user.toString() === req.user._id || req.user.rol === roles.admin) {
         await Review.findByIdAndDelete(req.params._id);
         return res.status(200).json({ message: "Review deleted successfully" });
     }
